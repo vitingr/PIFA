0a75b6700f2c3da780d41ada1ae86581fa593f03
vitingr <vitorgabrielsbo1460@gmail.com>
2023-02-01 20:55:57 -0300

    Alteração Gitignore

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
 
-    module.exports = {mongoURI: ""}
+    module.exports = {mongoURI: "mongodb://mongo:PPcwUIHJo4v4R86howLF@containers-us-west-110.railway.app:5519"}
 
 } else {
 
